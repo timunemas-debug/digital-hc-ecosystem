a85da7f90671ede2c6cc9c4004ffdeb538f79a3b
Jeremy <jeremyputradarma1@gmail.com>
2026-07-30 10:28:39 +0700

    Initial commit

---
+++ backend/.mvn/wrapper/src/main/java/com/digitalhc/DigitalHcEcosystemApplication.java
@@ -0,0 +1,13 @@
+package com.digitalhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalHcEcosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalHcEcosystemApplication.class, args);
+	}
+
+}
---
+++ backend/.mvn/wrapper/src/test/java/com/digitalhc/DigitalHcEcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalhc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalHcEcosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
