fe6f93cf5b70388fcf5a27ffbeebaa0ec21109f8
Jeremy <jeremyputradarma1@gmail.com>
2026-09-02 22:32:24 +0700

    feat: implement overtime service

--- backend/src/main/java/com/digitalhc/repository/OvertimeRepository.java
+++ backend/src/main/java/com/digitalhc/repository/OvertimeRepository.java
@@ -1,12 +1,22 @@
 package com.digitalhc.repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.digitalhc.model.Overtime;
+
+import jakarta.persistence.LockModeType;
 
 public interface OvertimeRepository extends JpaRepository <Overtime, Long>{
     
     boolean existsByEmployeeEmployeeIdAndDate(Long employeeId, LocalDate date);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT o FROM Overtime o WHERE o.overtimeId = :overtimeId")
+    Optional<Overtime> findByOvertimeIdWithLock(@Param("overtimeId") Long overtimeId);
 }
--- backend/src/main/java/com/digitalhc/service/OvertimeService.java
+++ backend/src/main/java/com/digitalhc/service/OvertimeService.java
@@ -1,6 +1,7 @@
 package com.digitalhc.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -16,6 +17,7 @@ import com.digitalhc.model.Attendance;
 import com.digitalhc.model.Employee;
 import com.digitalhc.model.OverTimeStatus;
 import com.digitalhc.model.Overtime;
+import com.digitalhc.model.Role;
 import com.digitalhc.repository.AttendanceRepository;
 import com.digitalhc.repository.EmployeeRepository;
 import com.digitalhc.repository.OvertimeRepository;
@@ -47,12 +49,16 @@ public class OvertimeService {
             throw new BadRequestException("Employee tidak memiliki attendance pada tanggal tersebut!");
         }
 
+        if (!request.getStartTime().toLocalDate().equals(request.getDate()) || !request.getEndTime().toLocalDate().equals(request.getDate())) {
+            throw new BadRequestException("Tanggal overtime tidak sama dengan start time dan end time!");
+        }
+
         if (!request.getStartTime().isBefore(request.getEndTime())) {
             throw new BadRequestException("Start time harus sebelum end time!");
         }
 
         if (overtimeRepository.existsByEmployeeEmployeeIdAndDate(employeeId, request.getDate())) {
-            throw new BadRequestException("Employe sudah memiliki overtime pada tanggal tersebut!");
+            throw new BadRequestException("Employee sudah memiliki overtime pada tanggal tersebut!");
         }
 
         Overtime overtime = overtimeMapper.toEntity(request);
@@ -65,5 +71,45 @@ public class OvertimeService {
         } catch (DataIntegrityViolationException e) {
             throw new BadRequestException("Employee sudah melakukan overtime hari ini!");
         }
+    }
+
+    public List<OvertimeResponse> getAllOvertime(){
+        return overtimeRepository.findAll()
+            .stream()
+            .map(overtimeMapper::toMapResponse)
+            .toList();
+    }
+
+    public OvertimeResponse getOvertimeById(Long overtimeId){
+
+        Overtime overtime = overtimeRepository.findById(overtimeId)
+                .orElseThrow(() -> new ResourceNotFound("Overtime dengan id tersebut tidak ditemukan!"));
+
+        return overtimeMapper.toMapResponse(overtime);
+    }
+
+    @Transactional
+    public void approvedOvertime(Long overtimeId, OverTimeStatus status){
+
+        Overtime overtime = overtimeRepository.findByOvertimeIdWithLock(overtimeId)
+                .orElseThrow(() -> new ResourceNotFound("Overtime tidak ditemukan!"));
+
+        if (overtime.getStatus() != OverTimeStatus.SUBMITTED) {
+            throw new BadRequestException("Status overtime sudah di proses dan tidak dapat diubah!");
+        }
+
+        if (status != OverTimeStatus.APPROVED && status != OverTimeStatus.REJECTED) {
+            throw new BadRequestException("Status hanya dapat menjadi approved atau rejected");
+        }
+
+        if (overtime.getApprovedBy() != null) {
+            throw new BadRequestException("Overtime sudah di proses!");
+        }
+
+        overtime.setApprovedBy(Role.ROLE_HC_MANAGER);
+        overtime.setStatus(status);
+        overtime.setApprovedAt(LocalDateTime.now());
+
+        overtimeRepository.save(overtime);
     }
 }
