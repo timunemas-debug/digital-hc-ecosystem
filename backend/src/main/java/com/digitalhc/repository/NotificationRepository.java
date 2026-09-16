8ac3710cbba5bea9aa2816b070813524debc8675
Jeremy <jeremyputradarma1@gmail.com>
2026-09-16 10:59:10 +0700

    feat: add notificationService

--- backend/src/main/java/com/digitalhc/repository/NotificationRepository.java
+++ backend/src/main/java/com/digitalhc/repository/NotificationRepository.java
@@ -1,9 +1,13 @@
 package com.digitalhc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.digitalhc.model.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
     
+    List<Notification> findByRecipientUserId(Long userId);
+    List<Notification> findByRecipientUserIdAndIsRead(Long userId, boolean isRead);
 }
--- backend/src/main/java/com/digitalhc/service/NotificationService.java
+++ backend/src/main/java/com/digitalhc/service/NotificationService.java
@@ -1,18 +1,75 @@
 package com.digitalhc.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.digitalhc.DTO.response.NotificationResponse;
+import com.digitalhc.exception.BadRequestException;
+import com.digitalhc.exception.ResourceNotFound;
 import com.digitalhc.mapper.NotificationMapper;
+import com.digitalhc.model.Notification;
+import com.digitalhc.model.User;
 import com.digitalhc.repository.NotificationRepository;
+import com.digitalhc.security.SecurityService;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class NotificationService {
     
     private final NotificationMapper notificationMapper;
     private final NotificationRepository notificationRepository;
+    private final UserService userService;
+    private final SecurityService securityService;
 
-    public NotificationService(NotificationMapper notificationMapper, NotificationRepository notificationRepository){
+    public NotificationService(NotificationMapper notificationMapper, NotificationRepository notificationRepository, UserService userService, SecurityService securityService){
         this.notificationMapper = notificationMapper;
         this.notificationRepository = notificationRepository;
+        this.userService = userService;
+        this.securityService = securityService;
+    }
+
+    public void createdNotification(Long userId, String message){
+
+        User user = userService.getUserById(userId);
+
+        Notification notification = new Notification();
+        notification.setRecipient(user);
+        notification.setMessage(message);
+        notification.setCreatedAt(LocalDateTime.now());
+        notification.setRead(false);
+
+        notificationRepository.save(notification);
+    }
+
+    @Transactional
+    public NotificationResponse markAsRead(Long notificationId){
+
+        Long userId = securityService.getCurrentUserId();
+
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new ResourceNotFound("Notification tidak ditemukan!"));
+
+        if (!notification.getRecipient().getUserId().equals(userId)) {
+            throw new BadRequestException("Notification bukan milik user!");
+        }
+
+        notification.setRead(true);
+
+        return notificationMapper.mapToResponse(notificationRepository.save(notification));
+    }
+
+    @Transactional
+    public void markAllRead(){
+        
+        Long userId = securityService.getCurrentUserId();
+
+        List<Notification> notifications = notificationRepository.findByRecipientUserIdAndIsRead(userId, false);
+
+        notifications.forEach(notification -> notification.setRead(true));
+
+        notificationRepository.saveAll(notifications);
     }
 }
